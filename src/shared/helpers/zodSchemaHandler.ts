0f858aa59aee08c0f98ceea88c8d526e4ae3afc8
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-04 09:29:31 -0300

    creating zod schema handler

---
+++ src/shared/helpers/zodSchemaHandler.ts
@@ -0,0 +1,11 @@
+import { ZodType } from "zod";
+
+export function zodSchemaHandler<T>(schema: ZodType<T>, data: T): T {
+  if (!data) throw new Error("requisição sem dados");
+  const validation = schema.safeParse(data);
+
+  if (validation.error)
+    throw new Error(`${validation.error?.issues[0].message}`);
+
+  return validation.data;
+}
