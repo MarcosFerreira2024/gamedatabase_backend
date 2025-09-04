45a0588e97c86f3d6927d4f9979d5dabc39cd513
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-04 10:14:58 -0300

    fixing path

--- src/modules/authentication/interfaces/IUserRepository.ts
+++
@@ -1,8 +0,0 @@
-import { User } from "../../../generated/prisma";
-
-interface IUserRepository {
-  findByEmail(email: string): Promise<User | null>;
-  create(email: string, password: string): Promise<void>;
-}
-
-export { IUserRepository };
--- src/shared/containers/index.ts
+++ src/shared/containers/index.ts
@@ -1,9 +1,9 @@
 import { container } from "tsyringe";
 import { UserRepository } from "../../modules/user/repositories/UserRepository";
-import { IUserRepository } from "../../modules/authentication/interfaces/IUserRepository";
 import { IPasswordHasherProvider } from "../../modules/authentication/interfaces/IPasswordHasherProvider";
 import { PasswordHasherProvider } from "../../modules/authentication/services/PasswordHasherProvider";
 import { TokenProvider } from "../../modules/authentication/services/TokenProvider";
+import { IUserRepository } from "../../modules/user/interfaces/IUserRepository";
 
 container.registerSingleton<IUserRepository>("UserRepository", UserRepository);
 container.registerSingleton<IPasswordHasherProvider>(
