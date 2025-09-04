abbd8318ba2e8df6257233fce394b7d5d1a3f358
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-04 09:28:43 -0300

    Create LoginUseCase.ts

---
+++ src/modules/authentication/useCases/LoginUseCase.ts
@@ -0,0 +1,31 @@
+import { inject, injectable } from "tsyringe";
+import { IPasswordHasherProvider } from "../interfaces/IPasswordHasherProvider";
+import { IUserRepository } from "../interfaces/IUserRepository";
+
+@injectable()
+class LoginUseCase {
+  constructor(
+    @inject("UserRepository") private userRepository: IUserRepository,
+    @inject("TokenProvider") private tokenProvider: ITokenProvider,
+    @inject("PasswordHasherProvider")
+    private passwordHasher: IPasswordHasherProvider
+  ) {}
+
+  async execute(email: string, password: string) {
+    const user = await this.userRepository.findByEmail(email);
+    if (!user) throw new Error("Esse usuário não existe, registre-se");
+    const passwordMatch = this.passwordHasher.compare(password, user.password);
+
+    if (!passwordMatch) throw new Error("Dados inválidos");
+
+    const token = this.tokenProvider.generateToken(email);
+    return token;
+  }
+
+  // verificar existencia do usuario
+  // comparar senhas
+  // gerar token
+  // retornar token
+}
+
+export { LoginUseCase };
