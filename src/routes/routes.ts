513774dfe89c619a094f5e23d2ef29befabca936
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-05 19:23:15 -0300

    Update routes.ts

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,10 +1,12 @@
 import { auth_routes } from "../modules/authentication/routes/route";
 import { Router } from "express";
 import { user_routes } from "../modules/user/routes/routes";
+import { catalog_routes } from "../modules/catalog/routes/route";
 
 const routes = Router();
 
 routes.use(auth_routes);
 routes.use("/user", user_routes);
+routes.use("/games", catalog_routes);
 
 export { routes };
