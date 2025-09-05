c489cad67032373b871a3b13ccb495a5b56d9dff
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-05 13:26:53 -0300

    Update route.ts

--- src/modules/authentication/routes/route.ts
+++ src/modules/authentication/routes/route.ts
@@ -1,11 +1,11 @@
 import { Router } from "express";
 import { AuthenticationController } from "../controllers/AuthenticationController";
 
-const auth_route = Router();
+const auth_routes = Router();
 
 const controller = new AuthenticationController();
 
-auth_route.post("/login", (req, res) => controller.login(req, res));
-auth_route.post("/signup", (req, res) => controller.signUp(req, res));
+auth_routes.post("/login", (req, res) => controller.defaultLogin(req, res));
+auth_routes.post("/signup", (req, res) => controller.signUp(req, res));
 
-export { auth_route };
+export { auth_routes };
