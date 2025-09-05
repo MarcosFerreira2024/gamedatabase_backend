e162829a644b0bcb2fd0d43d33039bc723304581
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-05 19:22:19 -0300

    Add catalog routes for games API

---
+++ src/modules/catalog/routes/route.ts
@@ -0,0 +1,16 @@
+import { Request, Response, Router } from "express";
+import { CatalogController } from "../controller/CatalogController";
+
+const catalog_routes = Router();
+
+const controller = new CatalogController();
+
+catalog_routes.get("/", (req: Request, res: Response) =>
+  controller.getGames(req, res)
+);
+
+catalog_routes.get("/:id", (req: Request, res: Response) =>
+  controller.getGameById(req, res)
+);
+
+export { catalog_routes };
