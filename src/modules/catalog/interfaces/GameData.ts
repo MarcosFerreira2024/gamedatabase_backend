8aca13c8432cbdd5b968f1f29275b54b6ddaa5f4
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-05 19:21:59 -0300

    Refactor GameData interface structure

--- src/modules/catalog/interfaces/GameData.ts
+++ src/modules/catalog/interfaces/GameData.ts
@@ -1,41 +1,33 @@
 export interface GameData {
+  id: number;
   igdbId: number;
   name: string;
-  description?: string;
-  storyline?: string;
-  series?: string;
-  franchises?: string[];
-  hypes?: number;
-
-  criticsScore?: number;
-  criticsCount?: number;
-  usersScore?: number;
-  usersCount?: number;
-  totalScore?: number;
-  totalCount?: number;
-
-  releaseDate?: Date | null;
-  status?: number;
-  category?: number;
-  updatedAt: Date;
-  createdAt: Date;
-
-  gameDevelopers: string[];
-  gamePublishers: string[];
-  gameGenres: string[];
-  gamePlatforms: string[];
-  gameModes: string[];
-  gameThemes: string[];
-  playerPerspectives: string[];
-  franchisesList?: string[];
-
-  cover: string;
-  artworks: string[];
-  screenshots: string[];
-  websites: string[];
-  videos: string[];
-
-  collections?: string[];
-  gameEngines?: string[];
+  summary: string | null;
+  storyline: string | null;
+  cover: string | null;
+  first_release_date: Date | null;
   url: string;
+  createdAt: string;
+  updatedAt: string;
+  criticsScore: number | null;
+  criticsCount: number | null;
+  usersScore: number | null;
+  usersCount: number | null;
+  totalScore: number | null;
+  totalCount: number | null;
+
+  artworks: { id: number; url: string; gameId: number }[];
+  collections: { id: number; name: string }[];
+  developers: { id: number; name: string }[];
+  gameEngines: { id: number; name: string }[];
+  franchises: { id: number; name: string }[];
+  genres: { id: number; name: string }[];
+  platforms: { id: number; name: string }[];
+  modes: { id: number; name: string }[];
+  screenshots: { id: number; url: string; gameId: number }[];
+  publishers: { id: number; name: string }[];
+  themes: { id: number; name: string }[];
+  videos: { id: number; name: string; url: string; gameId: number }[];
+  websites: { id: number; url: string; gameId: number }[];
+  playerPerspectives: { id: number; name: string }[];
 }
