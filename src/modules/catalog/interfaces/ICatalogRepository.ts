c31dc1afbfe92c306df7abcfd8f9b1ab8c83acd1
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-06 14:06:03 -0300

    Update ICatalogRepository interface methods

--- src/modules/catalog/interfaces/ICatalogRepository.ts
+++ src/modules/catalog/interfaces/ICatalogRepository.ts
@@ -1,7 +1,16 @@
-import { GameData } from "./GameData";
+import { Game } from "../../../generated/prisma";
 
 interface ICatalogRepository {
   saveAll(games: GameData[]): Promise<void>;
+  getByQuery(
+    searchParams: SearchParams | null,
+    sortParams: SortParams,
+    take: Take,
+    offset: number
+  ): Promise<FilteredGames[] | FilteredGames | null>;
+  getAll(): Promise<Game[]>;
+
+  getOneById(id: number): Promise<Game | null>;
 }
 
 export { ICatalogRepository };
