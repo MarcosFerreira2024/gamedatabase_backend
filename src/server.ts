ccf9ccb5d3a009a0390bb50efc58e366b8d8302b
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-16 00:01:30 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -6,13 +6,13 @@ import dotenv from "dotenv";
 import { routes } from "./routes/routes";
 import { container } from "tsyringe";
 import "./shared/containers";
-import "./job/syncCatalog";
+//import "./job/syncCatalog";
 
 dotenv.config();
 const port = process.env.PORT || 3000;
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: "http://localhost:5173" }));
 app.use(express.json());
 app.use(helmet());
 app.use(express.urlencoded({ extended: true }));
