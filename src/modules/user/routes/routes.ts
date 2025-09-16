009fd14d596924e5327b9ef49f5ecb3a8d406020
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-16 00:01:09 -0300

    new rest routes

--- src/modules/user/routes/routes.ts
+++ src/modules/user/routes/routes.ts
@@ -8,6 +8,33 @@ const controller = new UserController();
 user_routes.patch("/password", (req: Request, res: Response) =>
   controller.updatePassword(req, res)
 );
+
+user_routes.get("/", (req: Request, res: Response) =>
+  controller.userInfo(req, res)
+);
+
+user_routes.get("/wishlist", (req: Request, res: Response) =>
+  controller.getWishlist(req, res)
+);
+
+user_routes.post("/wishlist/:id", (req: Request, res: Response) =>
+  controller.addGameToWishlist(req, res)
+);
+
+user_routes.delete("/wishlist/:id", (req: Request, res: Response) =>
+  controller.removeGameFromWishlist(req, res)
+);
+
+user_routes.get("/favs", (req: Request, res: Response) =>
+  controller.getFavs(req, res)
+);
+user_routes.post("/fav/:id", (req: Request, res: Response) =>
+  controller.addGameToFav(req, res)
+);
+user_routes.delete("/fav/:id", (req: Request, res: Response) =>
+  controller.removeGameFromFav(req, res)
+);
+
 user_routes.patch("/email", (req: Request, res: Response) =>
   controller.updateEmail(req, res)
 );
