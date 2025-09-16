8b781b5ca26f57d75abb7d4a096d0c6833dccd13
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-16 00:02:08 -0300

    schemas (initial)

--- src/shared/schemas/schema.ts
+++ src/shared/schemas/schema.ts
@@ -1,15 +1,14 @@
 import { z } from "zod";
 
-// base
 export const EmailPasswordSchema = z.object({
   email: z.email(),
   password: z
     .string()
     .min(6, { message: "Sua senha deve conter no mínimo 6 caracteres" }),
 });
+
 export type EmailPassword = z.infer<typeof EmailPasswordSchema>;
 
-// user
 export const updateUserEmailSchema = EmailPasswordSchema.extend({
   newEmail: z.email(),
 });
@@ -19,8 +18,13 @@ export const updateUserPasswordSchema = EmailPasswordSchema.extend({
     .string()
     .min(6, { message: "Sua senha deve conter no mínimo 6 caracteres" }),
 });
+
+export const GameActionSchema = z.object({
+  email: z.email(),
+  gameId: z.number(),
+});
+
 export const deleteUserSchema = EmailPasswordSchema;
 
-// auth
 export const loginSchema = EmailPasswordSchema;
 export const signUpSchema = EmailPasswordSchema;
