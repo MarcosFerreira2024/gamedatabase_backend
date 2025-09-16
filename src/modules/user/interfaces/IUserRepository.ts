ca8bf95e807ab5e99f19ff95c8f6b6c1c335f1b7
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-16 00:00:58 -0300

    creating fav/wishlist interfaces/implementations

--- src/modules/user/interfaces/IUserRepository.ts
+++ src/modules/user/interfaces/IUserRepository.ts
@@ -1,4 +1,31 @@
-import { User } from "../../../generated/prisma";
+import { Prisma, User } from "../../../generated/prisma";
+
+export type Favs = {
+  favorites: {
+    igdbId: number;
+  }[];
+};
+
+type WishlistGame = {
+  name: string;
+  id: number;
+  igdbId: number;
+  summary: string | null;
+  storyline: string | null;
+  cover: string | null;
+  first_release_date: Date | null;
+  url: string | null;
+  createdAt: Date;
+  updatedAt: Date;
+  criticScore: number | null;
+  criticCount: number | null;
+  userScore: number | null;
+  userCount: number | null;
+  totalScore: number | null;
+  totalCount: number | null;
+};
+
+export type Wishlist = WishlistGame[];
 
 interface IUserRepository {
   findByEmail(email: string): Promise<User | null>;
@@ -6,6 +33,12 @@ interface IUserRepository {
   delete(email: string): Promise<void>;
   changePassword(email: string, newPassword: string): Promise<void>;
   changeEmail(email: string, newEmail: string): Promise<void>;
+  addGameToFav(email: string, gameId: number): Promise<void>;
+  removeGameFromFav(email: string, gameId: number): Promise<void>;
+  addGameToWishlist(email: string, gameId: number): Promise<void>;
+  removeGameFromWishlist(email: string, gameId: number): Promise<void>;
+  getFavs(email: string): Promise<number[] | null>;
+  getWishlist(email: string): Promise<Wishlist | null>;
 }
 
 export { IUserRepository };
--- src/modules/user/repositories/UserRepository.ts
+++ src/modules/user/repositories/UserRepository.ts
@@ -1,16 +1,102 @@
-import prisma from "../../../libs/prisma/prisma-client";
-import { IUserRepository } from "../interfaces/IUserRepository";
 import { User } from "../../../generated/prisma";
+import prisma from "../../../libs/prisma/prisma-client";
+import { Favs, IUserRepository, Wishlist } from "../interfaces/IUserRepository";
 
 class UserRepository implements IUserRepository {
+  async getFavs(email: string): Promise<number[] | null> {
+    const fav = await prisma.user.findMany({
+      where: {
+        email,
+      },
+      select: {
+        favorites: {
+          select: {
+            igdbId: true,
+          },
+        },
+      },
+    });
+
+    const favIds = fav.flatMap((user) => user.favorites.map((f) => f.igdbId));
+
+    return favIds;
+  }
+  async getWishlist(email: string): Promise<Wishlist | null> {
+    const wishlist = await prisma.user.findMany({
+      where: {
+        email,
+      },
+      select: {
+        wishlist: true,
+      },
+    });
+
+    const wishListNormalized = wishlist.flatMap((user) => user.wishlist);
+
+    return wishListNormalized;
+  }
   async findByEmail(email: string): Promise<User | null> {
-    const user = await prisma.user.findUnique({
+    return await prisma.user.findUnique({
       where: {
         email,
       },
     });
-
-    return user;
+  }
+  async addGameToFav(email: string, gameId: number): Promise<void> {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        favorites: {
+          connect: {
+            igdbId: gameId,
+          },
+        },
+      },
+    });
+  }
+  async addGameToWishlist(email: string, gameId: number): Promise<void> {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        wishlist: {
+          connect: {
+            igdbId: gameId,
+          },
+        },
+      },
+    });
+  }
+  async removeGameFromFav(email: string, gameId: number): Promise<void> {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        favorites: {
+          disconnect: {
+            igdbId: gameId,
+          },
+        },
+      },
+    });
+  }
+  async removeGameFromWishlist(email: string, gameId: number): Promise<void> {
+    await prisma.user.update({
+      where: {
+        email,
+      },
+      data: {
+        wishlist: {
+          disconnect: {
+            igdbId: gameId,
+          },
+        },
+      },
+    });
   }
 
   async create(email: string, password: string) {
