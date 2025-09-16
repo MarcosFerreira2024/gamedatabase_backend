5d68f2e4ba8e03e4a6618f62e40090584a555f29
Marcos Vinicius Ferreira Pinto <marcosfp2021@hotmail.com>
2025-09-15 23:59:12 -0300

    Update AuthenticationController.ts

--- src/modules/authentication/controllers/AuthenticationController.ts
+++ src/modules/authentication/controllers/AuthenticationController.ts
@@ -5,6 +5,7 @@ import { container } from "tsyringe";
 import { LoginUseCase } from "../useCases/LoginUseCase";
 import { SignUpUseCase } from "../useCases/SignUpUseCase";
 import { errorHandler } from "../../../shared/helpers/errorHandler";
+import { VerifyTokenUseCase } from "../useCases/VerifyTokenUseCase";
 
 class AuthenticationController {
   async defaultLogin(req: Request, res: Response) {
@@ -28,6 +29,18 @@ class AuthenticationController {
       await container.resolve(SignUpUseCase).execute(email, password);
 
       return res.status(200).json({ message: "Usuário criado com sucesso" });
+    } catch (e) {
+      errorHandler(e, res);
+    }
+  }
+  verifyToken(req: Request, res: Response) {
+    try {
+      const token = req.headers.authorization?.split(" ")[1];
+
+      if (!token) throw new Error("Token nao encontrado");
+
+      const result = container.resolve(VerifyTokenUseCase).execute(token);
+      return res.status(200).json({ message: result });
     } catch (e) {
       errorHandler(e, res);
     }
